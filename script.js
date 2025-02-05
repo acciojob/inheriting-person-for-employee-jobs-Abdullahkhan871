6683ed1c6c658419405991c77d60729594438c20
Abdullahkhan871 <123239280+Abdullahkhan871@users.noreply.github.com>
2025-02-05 14:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 
-function Employee(name, age, jobTitle) {}
+  this.greet = () => {
+    console.log(`Hello, my name is ${name}, I am ${age} years old.`);
+  };
+}
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+
+  this.jobGreet = () => {
+    console.log(
+      `Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`
+    );
+  };
+}
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
 // Do not change code below this line
 window.Person = Person;
